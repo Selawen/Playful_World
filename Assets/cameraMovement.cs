58b943128288f23d155e1eb4d3a86002926e217b
Selawen <37039183+Selawen@users.noreply.github.com>
2020-02-13 13:21:42 +0100

    Added some things
    materials
    scripts
    collision/physics

---
+++ Assets/cameraMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class cameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Now move!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (KeyCode==0x01)
+        
+
+    }
+}
---
+++ Assets/shootBullet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class shootBullet : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (KeyCode == 'a') {
+            rb.AddForce(0, 200, 0);
+        }
+    }
+}

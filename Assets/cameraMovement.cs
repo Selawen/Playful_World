7fa90ba8cbe0b90ff458a87d1e7d09885dacf1b6
Selawen <37039183+Selawen@users.noreply.github.com>
2020-02-20 13:18:29 +0100

    move
    fixed camera movement

--- Assets/cameraMovement.cs
+++ Assets/cameraMovement.cs
@@ -6,11 +6,15 @@ public class cameraMovement : MonoBehaviour
     public float speed = 10.0f;
     public float sideSpeed = 10.0f;
     public float horizontalLook = 2.0f;
+    public float verticalLook = 2.0f;
+    public float vRotation;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Now move!");
+        player = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -28,7 +32,21 @@ public class cameraMovement : MonoBehaviour
 
         //looking around with mouse
         float h = Input.GetAxis("Mouse X") * horizontalLook;
+        float v = Input.GetAxis("Mouse Y") * verticalLook;
 
-        transform.Rotate(0, h, 0);
+        vRotation -= v;
+
+        vRotation = Mathf.Clamp(vRotation, -45f, 45f);
+
+        transform.localRotation = Quaternion.Euler(vRotation, 0f, 0f);
+        player.transform.Rotate(0, h, 0);
+
+
+        /*float mouseX = Input.GetAxis("Mouse X") * MouseSensetivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensetivity * Time.deltaTime;
+        Xrotation -= mouseY;
+        Xrotation = Mathf.Clamp(Xrotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(Xrotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);*/
     }
 }

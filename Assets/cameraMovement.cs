4337e410caadb5b23f184bea5c050323c46fa4f0
Selawen <37039183+Selawen@users.noreply.github.com>
2020-02-24 18:16:00 +0100

    added reload
    added the possibility to reload

--- Assets/cameraMovement.cs
+++ Assets/cameraMovement.cs
@@ -8,13 +8,13 @@ public class cameraMovement : MonoBehaviour
     public float horizontalLook = 2.0f;
     public float verticalLook = 2.0f;
     public float vRotation;
-    public GameObject player;
+    public GameObject goPlayer;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Now move!");
-        player = GameObject.Find("player");
+        goPlayer = GameObject.Find("player");
     }
 
     // Update is called once per frame
@@ -27,8 +27,8 @@ public class cameraMovement : MonoBehaviour
         forward *= Time.deltaTime;
         sideways *= Time.deltaTime;
 
-        transform.Translate(0, 0, forward);
-        transform.Translate(sideways, 0, 0);
+        goPlayer.transform.Translate(0, 0, forward);
+        goPlayer.transform.Translate(sideways, 0, 0);
 
         //looking around with mouse
         float h = Input.GetAxis("Mouse X") * horizontalLook;
@@ -39,14 +39,6 @@ public class cameraMovement : MonoBehaviour
         vRotation = Mathf.Clamp(vRotation, -45f, 45f);
 
         transform.localRotation = Quaternion.Euler(vRotation, 0f, 0f);
-        player.transform.Rotate(0, h, 0);
-
-
-        /*float mouseX = Input.GetAxis("Mouse X") * MouseSensetivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * MouseSensetivity * Time.deltaTime;
-        Xrotation -= mouseY;
-        Xrotation = Mathf.Clamp(Xrotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(Xrotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);*/
+        goPlayer.transform.Rotate(0, h, 0);
     }
 }

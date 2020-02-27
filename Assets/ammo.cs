4a2910446a06d09427857127d068c59dd9650d34
Selawen <37039183+Selawen@users.noreply.github.com>
2020-02-27 11:54:53 +0100

    Added nature
    Added a low poly nature assets pack
    
    staying on trigger will reload again on click

--- Assets/ammo.cs
+++ Assets/ammo.cs
@@ -28,13 +28,21 @@ public class ammo : MonoBehaviour
         goBullet.GetComponent<shootBullet>().reset();
     }
 
+    public void OnTriggerStay(Collider other)
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            SetParent(goCamera);
+            goBullet.GetComponent<shootBullet>().reset();
+        }
+    }
+
+
     public void SetParent(GameObject newParent)
     {
         Debug.Log("entered");
         goBullet.transform.parent = newParent.transform;
         Debug.Log("parent = " + goBullet.transform.parent.name);
-
-
     }
 
 }
